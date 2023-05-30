cc8514b68bb279a932e6b73fd68db5d18c36af77
Anirudh Madhusudhan <65235028+anim1311@users.noreply.github.com>
2023-05-31 00:52:08 +0200

    Update settings.py

--- electronicsInventoryApp/settings.py
+++ electronicsInventoryApp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fj4c*kk&o+aqxdzu(s4sg=acs1dppqf*l5rj$%23@m##d9%c=&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','192.168.0.103','0.0.0.0','192.168.0.105']
 
